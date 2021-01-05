222a1f94c11bff73a185c055e4516b08bbc3134f
Shakh Nematov <waxboz.nematov@gmail.com>
2021-01-05 16:02:52 +0500

    Handling Events

--- documentation/src/App.js
+++ documentation/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import Jsx from "./docs/jsx";
 // import Welcome from "./docs/Components";
 // import State from "./docs/State";
+// import Events from './Main/Events'
 
 export default function MyApp() {
   return (
---
+++ documentation/src/Main/Events.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// 1. Handling Events
+// ***************************************
+// class Events extends React.Component {
+//
+//     alert = () => {
+//         console.log('this: ', this)
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 {/*<button onClick={this.alert}>Click me!</button>*/}
+//                 <button onClick={() => this.alert()}>Click me!</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default Events
+
+// ***************************************
