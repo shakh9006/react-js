4786cb42b5f66d2f1f7875e57991c9b50d78fa22
Shakh Nematov <waxboz.nematov@gmail.com>
2021-01-05 14:06:54 +0500

    Components and Props

--- documentation/src/App.js
+++ documentation/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Jsx from "./docs/jsx";
+// import Jsx from "./docs/jsx";
+// import Welcome from "./docs/Components";
 
 export default function MyApp() {
   return (
       <div>
-          <Jsx/>
+
       </div>
   )
 }
---
+++ documentation/src/docs/Components.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// 1. Function and Class Components
+// ***************************************
+// function Welcome({ name }) {
+//     return (<h1>Hello, {name}</h1>)
+// }
+
+// class Welcome extends React.Component {
+//     render() {
+//         return (<h1>Hello, {this.props.name}</h1>)
+//     }
+// }
+//
+// export default Welcome
+// ***************************************
